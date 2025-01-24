ec9c18e699bd9dbc603ddb74ea267c68deb64803
JFikents <138692812+JorFik@users.noreply.github.com>
2025-01-24 17:46:29 +0100

    Add basic user registration functionality with form handling

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('users/', views.UserList.as_view(), name='user-list'),
+	path('register/', views.register, name='user-registration'),
 ]
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 class UserList(generics.ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			print(form)
+			return redirect('/users/')
+		else:
+			print(form.errors)
+	form = UserCreationForm()
+	return render(request, 'register.html', { 'form': form })
