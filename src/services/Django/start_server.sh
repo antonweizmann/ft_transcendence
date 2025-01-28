5ddb6c12a4ad748c7b94bb0838678bf22fe1aa62
JFikents <138692812+JorFik@users.noreply.github.com>
2025-01-28 19:53:38 +0100

    Simplifying into a single command

--- src/services/Django/start_server.sh
+++ src/services/Django/start_server.sh
@@ -8,15 +8,16 @@ source /run/secrets/django_superuser
 echo "Migrating Django database..."
 python manage.py migrate
 
-echo "Creating Django superuser..."
-python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --noinput
-echo "Setting superuser password..."
-python manage.py shell -c \
-"from django.contrib.auth import get_user_model;\
-User = get_user_model();\
-user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME');\
-user.set_password('$DJANGO_SUPERUSER_PASSWORD');\
-user.save()"
+echo "Checking for superuser..."
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+	print('Creating superuser...')
+	User.objects.create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')
+else:
+	print('Superuser already exists.')
+"
 
 echo "Starting Django server..."
 exec python manage.py runserver 0.0.0.0:8000
