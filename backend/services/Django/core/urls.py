397f6a16c40c013762275dc1f9013b153a29258e
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-03 15:25:53 +0100

    Fix /play/ issue in backend

--- backend/services/Django/core/urls.py
+++ backend/services/Django/core/urls.py
@@ -32,6 +32,7 @@ def redirect_to_home(request):
 urlpatterns = [
 	path('', pong_game, name='pong_game'),
 	path('home/', redirect_to_home, name='home'),
+	path('play/', redirect_to_home, name='play'),
 	path('admin/', admin.site.urls),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
