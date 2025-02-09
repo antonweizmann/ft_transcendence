36ef48f0e07d9ecee1e985f55a8a55be588317d8
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-09 17:44:40 +0100

    Django sending links to https

--- backend/services/Django/core/settings.py
+++ backend/services/Django/core/settings.py
@@ -162,3 +162,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
