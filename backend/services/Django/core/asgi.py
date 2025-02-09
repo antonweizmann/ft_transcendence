0232c9dd4db6cf26f8057add30437e295ce56ab2
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-09 17:28:34 +0100

    Fix wss

--- backend/services/Django/core/asgi.py
+++ backend/services/Django/core/asgi.py
@@ -8,16 +8,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application # type: ignore
-from django.urls import path # type: ignore
-from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
-from channels.auth import AuthMiddlewareStack # type: ignore
-from pong.consumers import PongConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
+from channels.auth import AuthMiddlewareStack # type: ignore
+from django.urls import path # type: ignore
+from pong.consumers import PongConsumer
 
 application = ProtocolTypeRouter({
 	'http': django_asgi_app,
