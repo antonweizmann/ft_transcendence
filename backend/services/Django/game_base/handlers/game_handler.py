6dd1c727c78423399d12f253dcb087eb6935bfe8
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-13 21:20:30 +0100

    Abstract handlers + Pong countdown + Paddle movement Fix

--- backend/services/Django/game_base/handlers/game_handler.py
+++ backend/services/Django/game_base/handlers/game_handler.py
@@ -1,165 +1,51 @@
-import json
 import threading
-import inspect
 
-from typing import Protocol, Optional, List, Dict
 from game_base.models import GameMatchBase
 from django.contrib.auth import get_user_model # type: ignore
+from .core_base_handler import SendFunc, CoreHandlerBase
 
 Player = get_user_model()
 
-class SendFunc(Protocol):
-	def __call__(self, message, error: bool = False) -> None:
-		pass
-
-class GameHandlerBase:
+class GameHandlerBase(CoreHandlerBase):
 	class Meta:
 		abstract = True
 
-	required_players = 1
-
-	def __new__(cls, *args, **kwargs):
-		stack = inspect.stack()
-		try:
-			caller = stack[2].function
-		except IndexError:
-			caller = ''
-		if caller != 'get_game':
-			raise TypeError("Instances of any GameHandler can only be "\
-					"created using the GameManager.get_game().")
-		return super(GameHandlerBase, cls).__new__(cls)
+	_handler_type: str	= 'Game'
+	_subtype: str		= 'Unknown'
 
 	def __init__(self, game_id: str):
-		self.results: type[GameMatchBase] | None = None
-		self.is_game_running = False
-		self.send_func : Optional[SendFunc] = None
-		self.game_type : str = 'Unknown'
-		self.game_id = game_id
-		self.game_state = { 'score': {} }
-		self.players: List[Player] = []
-		self._indexes: Dict[int, Player] = {}
+		super().__init__(game_id)
+		self._model: type[GameMatchBase] | None	= None
+		self._id								= game_id
+		self._state								= {'score': {}}
 
-	def _get_index(self, player: Player)-> int:
-		player_index = 0
-		while (self._indexes.get(player_index) is not None
-			and player is not self._indexes[player_index]):
-			player_index += 1
-		if player_index not in self._indexes:
-			self._indexes[player_index] = player
-		return player_index
+	def join_match(self, player: Player, send_func: SendFunc) -> int | None: # type: ignore
+		return super()._join(player, send_func)
 
-	def join_match(self, player: Player, send_func: SendFunc) -> int | None:
-		if not self._allowed_to_join(player, send_func):
-			return
-		if self.results is None:
-			raise ValueError('Results object must be set in __init__ from the child class.')
-		self.results.players.add(player)
-		self.players.append(player)
-		player_index = self._get_index(player)
-		self.send_func = send_func
-		self._send_lobby_update()
-		return player_index
+	def leave_match(self, player: Player): # type: ignore
+		super()._leave(player)
 
-	def leave_match(self, player: Player):
-		if player not in self.players:
-			return
-		self.players.remove(player)
-		if self.results.status == 'waiting':
-			self.results.players.remove(player)
-			for index, p in self._indexes.items():
-				if p == player:
-					player_index = index
-					break
-			if player_index is not None:
-				del self._indexes[player_index]
-		if self.send_func is None:
-			return
-		self._send_lobby_update()
-
-	def _send_lobby_update(self):
-		self.send_func(json.dumps({
-			'type': 'lobby_update',
-			'game_id': self.game_id,
-			'players': [{'index': index, 'username': player.username} for\
-				index, player in self._indexes.items()]
-		}))
-
-	def start_game(self, player_index):
-		if not self._allowed_to_start(player_index):
-			return
-		self.game_state['score'] = {player.__str__(): 0 for player in self.players}
-		self.is_game_running = True
-		self.results.status = 'in_progress'
-		self.results.save()
+	def start_game(self, player_index: int):
 		game_thread = threading.Thread(target=self._run_game)
-		game_thread.start()
+		self._state['score'] = {player.__str__(): 0 for player in self.players}
+		super()._start(player_index, game_thread.start)
 
 	def _send_game_state(self):
-		if self.send_func is None:
-			raise ValueError('You must join a match before sending game state.')
-		self.send_func(json.dumps({
-			'type': f'{self.game_type}_update',
-			'game_id': self.game_id,
-			'game_state': self.game_state
-		}))
-
-	def _allowed_to_join(self, player: Player, send_func: SendFunc):
-		if self.results.status == 'finished':
-			send_func(json.dumps({
-				'type': 'error',
-				'message': 'Game has already finished.'
-			}), True)
-			return False
-		if self.is_game_running:
-			send_func(json.dumps({
-				'type': 'error',
-				'message': 'Game has already started.'
-			}), True)
-			return False
-		if player in self.players:
-			send_func(json.dumps({
-				'type': 'error',
-				'message': 'Player already joined.'
-			}), True)
-			return False
-		if len(self.players) >= self.required_players:
-			send_func(json.dumps({
-				'type': 'error',
-				'message': 'Game lobby is full.'
-			}), True)
-			return False
-		return True
-	
-	def _allowed_to_start(self, player_index: int) -> bool:
-		if self.send_func is None or player_index == None:
-			raise ValueError('You must join a match before starting the game.')
-		if self.results.status == 'finished':
-			self.send_func(json.dumps({
-				'type': 'error',
-				'message': 'Game has already finished.'
-			}), True)
-			return False
-		if self.required_players != len(self.players):
-			self.send_func(json.dumps({
-				'type': 'error',
-				'message': 'Waiting for other players to join.'
-			}))
-			return False
-		return True
+		super()._send_state()
 
 	def _run_game(self):
 		raise NotImplementedError
 
 	def _update_game_state(self):
 		raise NotImplementedError
-	
+
 	def _check_win_conditions(self):
 		raise NotImplementedError
 
 # Don't forget to add the following code to the child class when the game is finished:
-		self.is_game_running = False
-		self.results.status = 'finished'
-		self.results.save()
+		self._is_active = False
+		self._model.status = 'finished'
+		self._model.save()
 
 	def move(self, player_index, move):
 		raise NotImplementedError
--- backend/services/Django/pong/pong_handler.py
+++ backend/services/Django/pong/pong_handler.py
@@ -23,99 +23,109 @@ MAX_PLAYER_Y = BOARD_HEIGHT - 50
 PLAYER_SPEED = 10
 
 class PongHandler(GameHandlerBase):
-	required_players = 2
+	_subtype			= 'Pong'
+	_required_players	= 2
 
 	def __set_random_ball_direction(self) -> list[int, int]:
 		return [random.choice([-1, 1]), random.choice([-1, 1])]
 
 	def __init__(self, game_id: str):
 		super().__init__(game_id)
-		self.results = PongMatch.objects.create()
-		self.game_type = 'pong'
-		self.game_state = {
+		self._model = PongMatch.objects.create()
+		self._state.update({
 			'ball_position': INITIAL_BALL_POSITION.copy(),
 			'ball_direction': self.__set_random_ball_direction(),
 			'paddle_1_position': 250,
 			'paddle_2_position': 250,
-		}
-		self.ball_speed = BALL_SPEED
+		})
+		self._ball_speed = BALL_SPEED
+
+	def __start_countdown(self):
+		i = 0
+		for i in range(3, 0, -1):
+			self._send_func(json.dumps({
+				'message': f'Game starting in {i}...'
+			}))
+			time.sleep(1)
 
 	def _run_game(self):
 		target_fps = 60
 		target_frame_duration = 1 / target_fps
 
-		while self.is_game_running:
+		self.__start_countdown()
+		while self._is_active:
 			start_time = time.time()
 
 			self._update_game_state()
-			self._send_game_state()
+			if self._is_active:
+				self._send_game_state()
 
 			elapsed_time = time.time() - start_time
 			sleep_time = max(0, target_frame_duration - elapsed_time)
 			time.sleep(sleep_time)
 
 	def __reset_ball(self):
-		self.game_state['ball_position'] = INITIAL_BALL_POSITION.copy()
-		self.game_state['ball_direction'] = self.__set_random_ball_direction()
-		self.ball_speed = BALL_SPEED
+		self._state['ball_position'] = INITIAL_BALL_POSITION.copy()
+		self._state['ball_direction'] = self.__set_random_ball_direction()
+		self._ball_speed = BALL_SPEED
 
 	def __score_goal(self):
-		if self.game_state['ball_position'][X] <= MIN_BALL_X:
-			self.game_state['score'][self.players[1].__str__()] += 1
-		elif self.game_state['ball_position'][X] >= MAX_BALL_X:
-			self.game_state['score'][self.players[0].__str__()] += 1
-		self.results.scores = self.game_state['score']
-		self.results.save()
+		if self._state['ball_position'][X] <= MIN_BALL_X:
+			self._state['score'][self.players[1].__str__()] += 1
+		elif self._state['ball_position'][X] >= MAX_BALL_X:
+			self._state['score'][self.players[0].__str__()] += 1
+		self._model.scores = self._state['score']
+		self._model.save()
 
 	def __check_for_collisions(self):
-		if (self.game_state['ball_position'][Y] <= MIN_BALL_Y
-			or self.game_state['ball_position'][Y] >= MAX_BALL_Y):
-			self.game_state['ball_direction'][Y] *= -1
+		if (self._state['ball_position'][Y] <= MIN_BALL_Y
+			or self._state['ball_position'][Y] >= MAX_BALL_Y):
+			self._state['ball_direction'][Y] *= -1
 
-		if (self.game_state['ball_position'][X] <= MIN_BALL_X
-			or self.game_state['ball_position'][X] >= MAX_BALL_X):
+		if (self._state['ball_position'][X] <= MIN_BALL_X
+			or self._state['ball_position'][X] >= MAX_BALL_X):
 			self.__score_goal()
 			self.__reset_ball()
 
 	def __bounce_from_paddle(self, paddle_position: float):
-		self.game_state['ball_direction'][X] *= -1
-		self.game_state['ball_direction'][Y] = (
-			self.game_state['ball_position'][Y] - paddle_position) / 50
-		if self.ball_speed < 11:
-			self.ball_speed += 0.66
+		self._state['ball_direction'][X] *= -1
+		self._state['ball_direction'][Y] = (
+			self._state['ball_position'][Y] - paddle_position) / 50
+		if self._ball_speed < 11:
+			self._ball_speed += 0.66
 
 	def __check_paddle_collisions(self):
 		# Paddle 1 collision
-		if (self.game_state['ball_position'][X] <= 20
-			and self.game_state['ball_position'][Y] >= self.game_state['paddle_1_position'] - 50
-			and self.game_state['ball_position'][Y] <= self.game_state['paddle_1_position'] + 50):
-			self.__bounce_from_paddle(self.game_state['paddle_1_position'])
+		if (self._state['ball_position'][X] <= 20
+			and self._state['ball_position'][Y] >= self._state['paddle_1_position'] - 50
+			and self._state['ball_position'][Y] <= self._state['paddle_1_position'] + 50):
+			self.__bounce_from_paddle(self._state['paddle_1_position'])
 
 		# Paddle 2 collision
-		if (self.game_state['ball_position'][X] >= 780
-			and self.game_state['ball_position'][Y] >= self.game_state['paddle_2_position'] - 50
-			and self.game_state['ball_position'][Y] <= self.game_state['paddle_2_position'] + 50):
-			self.__bounce_from_paddle(self.game_state['paddle_2_position'])
+		if (self._state['ball_position'][X] >= 780
+			and self._state['ball_position'][Y] >= self._state['paddle_2_position'] - 50
+			and self._state['ball_position'][Y] <= self._state['paddle_2_position'] + 50):
+			self.__bounce_from_paddle(self._state['paddle_2_position'])
 
 	def __move_ball(self):
-		magnitude = sqrt(self.game_state['ball_direction'][X] ** 2
-			+ self.game_state['ball_direction'][Y] ** 2)
-		normalized_direction = [self.game_state['ball_direction'][X] / magnitude,
-			self.game_state['ball_direction'][Y] / magnitude]
-		self.game_state['ball_position'][X] += normalized_direction[X] * BALL_SPEED
-		self.game_state['ball_position'][Y] += normalized_direction[Y] * BALL_SPEED
+		magnitude = sqrt(self._state['ball_direction'][X] ** 2
+			+ self._state['ball_direction'][Y] ** 2)
+		normalized_direction = [self._state['ball_direction'][X] / magnitude,
+			self._state['ball_direction'][Y] / magnitude]
+		self._state['ball_position'][X] += normalized_direction[X] * BALL_SPEED
+		self._state['ball_position'][Y] += normalized_direction[Y] * BALL_SPEED
 
 	def __check_win_conditions(self):
-		if (self.game_state['score'][self.players[1].__str__()] >= 10
-				or self.game_state['score'][self.players[0].__str__()] >= 10):
-			self.send_func(json.dumps({
+		if (self._state['score'][self.players[1].__str__()] >= 10
+				or self._state['score'][self.players[0].__str__()] >= 10):
+			self._send_func(json.dumps({
 				'type': 'game_over',
-				'game_id': self.game_id,
-				'game_state': self.game_state
+				'game_id': self._id,
+				'game_state': self._state
 			}))
-			self.is_game_running = False
-			self.results.status = 'finished'
-			self.results.save()
+			self._is_active = False
+			self._model.status = 'finished'
+			self._model.save()
 
 
 	def _update_game_state(self):
@@ -129,7 +139,15 @@ class PongHandler(GameHandlerBase):
 			raise ValueError('Invalid player index.')
 		if move not in ['up', 'down']:
 			raise ValueError('Invalid move.')
-		if move == 'up':
-			self.game_state[f'paddle_{player_index + 1}_position'] -= PLAYER_SPEED
-		elif move == 'down':
-			self.game_state[f'paddle_{player_index + 1}_position'] += PLAYER_SPEED
+		if (move == 'up' and
+			self._state[f'paddle_{player_index + 1}_position'] >=
+			MIN_PLAYER_Y + PLAYER_SPEED):
+			self._state[f'paddle_{player_index + 1}_position'] -= PLAYER_SPEED
+		elif move == 'up':
+			self._state[f'paddle_{player_index + 1}_position'] = MIN_PLAYER_Y
+		elif (move == 'down' and
+			self._state[f'paddle_{player_index + 1}_position'] <=
+			MAX_PLAYER_Y - PLAYER_SPEED):
+			self._state[f'paddle_{player_index + 1}_position'] += PLAYER_SPEED
+		else:
+			self._state[f'paddle_{player_index + 1}_position'] = MAX_PLAYER_Y
