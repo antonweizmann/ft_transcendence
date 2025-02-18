c3bec90b0e8bbcd240cf5dba8957e6abbd173571
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-18 17:36:10 +0100

    Now inherits from core base consumer

--- backend/services/Django/game_base/consumers/game_base_consumer.py
+++ backend/services/Django/game_base/consumers/game_base_consumer.py
@@ -6,109 +6,38 @@ from channels.exceptions import StopConsumer # type: ignore
 from django.contrib.auth import get_user_model # type: ignore
 from game_base.managers import GameManager
 from game_base.handlers import GameHandlerBase
+from .core_base_consumer import CoreBaseConsumer
 
 Player = get_user_model()
 
-class GameBaseConsumer(WebsocketConsumer):
+class GameBaseConsumer(CoreBaseConsumer):
 	class Meta:
 		abstract = True
 
+	_type		= 'Game'
+	_subtype	= 'Base'
+
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.game_handler: type[GameHandlerBase] | None = None
-		self.game_manager = GameManager()
-		self.game_id = None
-		self.player = None
-		self.player_index = None
-		self.in_Match = False
-
-	def connect(self):
-		super().connect()
-		self.send(json.dumps({
-			'message': 'Connected to server.'
-		}))
-
-	def send_to_group(self, message, error: bool = False) -> None:
-		try:
-			if error:
-				self.send(json.dumps(message))
-				return
-			async_to_sync(self.channel_layer.group_send)(self.game_id, {
-				'type': 'group.message',
-				'message': message
-			})
-		except Exception as e:
-			print("Error sending message to group:", e)
-			raise StopConsumer()
-
-	def group_message(self, event):
-		message = event['message']
-		self.send(text_data=json.dumps(message))
-
-	def _join_lobby(self, player, game_id, game_handler: type[GameHandlerBase]):
-		if self.in_Match:
-			self.send(json.dumps({
-				'message': 'You are already in a match.'
-			}))
-			return
-		self.player = player
-		self.game_id = game_id
-		self.game_handler = self.game_manager.get_game(game_handler, game_id)
-		try:
-			async_to_sync(self.channel_layer.group_add)(game_id, self.channel_name)
-		except TypeError as e:
-			self.send(json.dumps({'error': str(e)}))
-			return
-		self.player_index = self.game_handler.join_match(player,
-			self.send_to_group)
-		if self.player_index is None:
-			self.send(json.dumps({
-				'message': 'Failed to join lobby.'
-			}))
-			async_to_sync(self.channel_layer.group_discard)(game_id,
-				self.channel_name)
-			return
-		self.in_Match = True
+		self._handler: type[GameHandlerBase] | None	= None
+		self._manager								= GameManager()
 
 	def receive(self, text_data):
-		text_data_json = json.loads(text_data)
-		action = text_data_json.get('action')
-		player_pk = text_data_json.get('player_pk')
-		game_id = text_data_json.get('game_id')
+		text_data_json, action = super().receive(text_data)
 
-		if action == 'join_lobby':
-			try:
-				player_pk = int(player_pk)
-				self.player = Player.objects.get(pk=player_pk)
-				if self.player == None:
-					raise Player.DoesNotExist
-			except Player.DoesNotExist:
-				self.send(json.dumps({'message': 'Player not found.'}))
-				return
-			except ValueError as e:
-				self.send(json.dumps({'message': 'Invalid player ID.'}))
-				return
-			self.join_lobby(self.player, game_id)
+		if action == None:
+			return
 
-		elif action == 'start_game':
-			if not self.game_handler:
-				self.send(json.dumps({'message': 'You are not in a game.'}))
-				return
-			try:
-				self.game_handler.start_game(self.player_index)
-			except ValueError as e:
-				self.send(json.dumps({'message': str(e)}))
-
-		elif action == 'move':
+		if action == 'move':
 			move = text_data_json.get('move')
-			if not self.game_handler:
+			if not self._handler:
 				self.send(json.dumps({'message': 'You are not in a game.'}))
 				return
 			self.send(json.dumps({
 				'message': f'Move: {move}, Player index: {self.player_index}'
 			}))
 			try:
-				self.game_handler.move(self.player_index, move)
+				self._handler.move(self.player_index, move)
 			except ValueError as e:
 				self.send(json.dumps({'message': str(e)}))
 
@@ -117,15 +46,3 @@ class GameBaseConsumer(WebsocketConsumer):
 			self.send(json.dumps({
 				'message': message
 			}))
-
-	def disconnect(self, close_code):
-		if self.game_handler:
-			async_to_sync(self.channel_layer.group_discard)(self.game_id, self.channel_name)
-			self.game_handler.leave_match(self.player)
-			if len(self.game_handler.players) == 0 and self.game_id:
-				self.game_manager.remove_game(self.game_id)
-
-	def join_lobby(self, player, game_id):
-		raise NotImplementedError('You must implement this method in the child'
-							+ ' class to specify the game handler.')
-		return self._join_lobby(player, game_id, GameHandlerBase)
