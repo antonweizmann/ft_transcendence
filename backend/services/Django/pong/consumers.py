3e664bdbb13076d59ed92ac75ebc502b2d71dffe
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-18 17:36:32 +0100

    Establish _subtype

--- backend/services/Django/pong/consumers.py
+++ backend/services/Django/pong/consumers.py
@@ -2,5 +2,7 @@ from game_base.consumers import GameBaseConsumer
 from pong.pong_handler import PongHandler
 
 class PongConsumer(GameBaseConsumer):
+	_subtype = 'Pong'
+
 	def join_lobby(self, player, game_id):
 		return super()._join_lobby(player, game_id, PongHandler)
