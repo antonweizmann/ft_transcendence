20370ffe2a63e6fe82dadd805a34ad57203bdba7
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-18 18:01:13 +0100

    First Tournament consumer base

--- backend/services/Django/game_base/consumers/tournament_base_consumer.py
+++ backend/services/Django/game_base/consumers/tournament_base_consumer.py
@@ -1,13 +1,29 @@
-from channels.generic.websocket import WebsocketConsumer # type: ignore
-from game_base.managers import TournamentManager
+import json
 
-class TournamentBaseConsumer(WebsocketConsumer):
+from game_base.managers import TournamentManager
+from game_base.handlers import TournamentHandlerBase
+from .core_base_consumer import CoreBaseConsumer
+
+class TournamentBaseConsumer(CoreBaseConsumer):
 	class Meta:
 		abstract = True
 
+	_type		= 'Tournament'
+	_subtype	= 'Base'
+
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.tournament_manager = TournamentManager()
-		self.tournament_id = None
-		self.player = None
-		self.in_tournament = False
+		self._handler: type[TournamentHandlerBase] | None	= None
+		self._manager										= TournamentManager()
+
+	def receive(self, text_data):
+		text_data_json, action = super().receive(text_data)
+
+		if action == None:
+			return
+
+		else:
+			message = text_data_json.get('message')
+			self.send(json.dumps({
+				'message': message
+			}))
