8118111cb7c0a8785677ba344e01e32c8b0e609e
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-23 16:36:51 +0100

    Fix missing inheritance

--- backend/services/Django/game_base/managers/tournament_manager.py
+++ backend/services/Django/game_base/managers/tournament_manager.py
@@ -1,7 +1,8 @@
 from channels.generic.websocket import WebsocketConsumer # type: ignore
 from game_base.handlers import TournamentHandlerBase
+from .manager_base import ManagerBase
 
-class TournamentManager:
+class TournamentManager(ManagerBase):
 
 	def get_tournament(self, tournament_handler: type[TournamentHandlerBase], tournament_id: str):
 		self._get_object(tournament_handler, tournament_id)
