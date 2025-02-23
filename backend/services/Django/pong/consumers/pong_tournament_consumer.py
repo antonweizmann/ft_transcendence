9119de9600ce0ab5a2641eab8258497a1afeeac2
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-23 16:06:06 +0100

    Clean up

--- backend/services/Django/pong/consumers/pong_tournament_consumer.py
+++ backend/services/Django/pong/consumers/pong_tournament_consumer.py
@@ -4,9 +4,5 @@ from pong.handlers import PongTournamentHandler
 class PongTournamentConsumer(TournamentBaseConsumer):
 	_subtype = 'Pong'
 
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self._handler: type[PongTournamentHandler] | None = None
-
 	def join_lobby(self, object_id):
 		return super().join_lobby(object_id, PongTournamentHandler)
