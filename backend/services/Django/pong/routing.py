1896dba93e65720afd8db93167249d33a3a9dbaa
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-23 16:35:57 +0100

    Added routes for tournament websocket

--- backend/services/Django/pong/routing.py
+++ backend/services/Django/pong/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path # type: ignore
-from .consumers import PongConsumer
+from .consumers import PongConsumer, PongTournamentConsumer
 
 pong_ws_urlpatterns = [
 	path('ws/pong/', PongConsumer.as_asgi()),
 	path('ws/pong/<str:game_id>/', PongConsumer.as_asgi()),
-	# path('ws/tournament/<str:tournament_id>/', PongConsumer.as_asgi()),
+	path('ws/tournament/<str:tournament_id>/', PongTournamentConsumer.as_asgi()),
+	path('ws/tournament/', PongTournamentConsumer.as_asgi()),
 ]
