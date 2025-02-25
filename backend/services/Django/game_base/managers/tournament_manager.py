5decca9310ecc2bb6f9e1791c72981ae3f05c3ee
JFikents <138692812+JorFik@users.noreply.github.com>
2025-02-25 18:50:08 +0100

    Fix Returning None

--- backend/services/Django/game_base/managers/tournament_manager.py
+++ backend/services/Django/game_base/managers/tournament_manager.py
@@ -5,9 +5,9 @@ from .manager_base import ManagerBase
 class TournamentManager(ManagerBase):
 
 	def get_tournament(self, tournament_handler: type[TournamentHandlerBase], tournament_id: str):
-		self._get_object(tournament_handler, tournament_id)
+		return self._get_object(tournament_handler, tournament_id)
 
 	def remove_tournament(self, tournament_id: str):
-		self._remove_object(tournament_id)
+		return self._remove_object(tournament_id)
 
 tournament_manager = TournamentManager()
