2801f27458378d91bd788b49586a884c01543a7c
JFikents <138692812+JorFik@users.noreply.github.com>
2025-03-01 17:51:53 +0100

    Tournament now thread safe

--- backend/services/Django/pong/handlers/tournament.py
+++ backend/services/Django/pong/handlers/tournament.py
@@ -19,19 +19,23 @@ class PongTournamentHandler(TournamentHandlerBase):
 
 	def _set_matches(self, players_without_match: list[Player]): # type: ignore
 		random.shuffle(players_without_match)
-		if self._required_players % 2 != 0:
-			extra_player = random.choice(players_without_match)
-		required_matches = self._required_players // 2
+		with self._lock:
+			if self._required_players % 2 != 0:
+				extra_player = random.choice(players_without_match)
+			required_matches = self._required_players // 2
 		for i in range(required_matches):
 			player_1 = players_without_match.pop()
 			player_2 = players_without_match.pop()
-			self._state['pending_matches'].append([player_1, player_2])
+			with self._lock:
+				self._state['pending_matches'].append([player_1, player_2])
 		if extra_player:
-			self._state['pending_matches'].append([extra_player, None])
+			with self._lock:
+				self._state['pending_matches'].append([extra_player, None])
 
 	def leave_tournament(self, player: Player): # type: ignore
 		super().leave_tournament(player)
-		self._state['pending_matches'] = []
+		with self._lock:
+			self._state['pending_matches'] = []
 
 	def _start_tournament(self, player_index: int):
 		if not self._allowed_to_start(player_index):
@@ -41,43 +45,54 @@ class PongTournamentHandler(TournamentHandlerBase):
 
 	def __qualify_players(self) -> set[Player]: # type: ignore
 		players_without_match = set()
-		max_score = max(self._state['leaderboard'].values())
-		for match in self._state['finished_matches']:
-			if any(self._state['leaderboard'][player] != max_score for player in match.keys()):
-				continue
-			winner = max(match, key=match.get)
-			players_without_match.add(winner)
-			self._state['leaderboard'][winner] += 1
+		with self._lock:
+			max_score = max(self._state['leaderboard'].values())
+			for match in self._state['finished_matches']:
+				if any(self._state['leaderboard'][player] != max_score for player in match.keys()):
+					continue
+				winner = max(match, key=match.get)
+				players_without_match.add(winner)
+				self._state['leaderboard'][winner] += 1
 		return players_without_match
 
 	def _set_next_matches(self, match: list[Player, Player]): # type: ignore
-		if len(self._state['pending_matches']) != 0:
-			raise ValueError('There are still pending matches')
-		players_without_match = self.__qualify_players()
-		if match[1] is None:
-			extra_player = match[0]
-			self._state['leaderboard'][extra_player] += 1
-			players_without_match.add(match[0])
-		self._set_matches(list(players_without_match))
-		if len(self._state['pending_matches']) == 1 and self._state['pending_matches'][0][1] is None:
-			self._is_active = False
+		with self._lock:
+			if len(self._state['pending_matches']) != 0:
+				raise ValueError('There are still pending matches')
+			players_without_match = self.__qualify_players()
+			if match[1] is None:
+				extra_player = match[0]
+				self._state['leaderboard'][extra_player] += 1
+				players_without_match.add(match[0])
+			self._set_matches(list(players_without_match))
+			if len(self._state['pending_matches']) == 1 and self._state['pending_matches'][0][1] is None:
+				self._is_active = False
 
 	def _update_game_state(self, match: list[Player, Player]): # type: ignore
-		match_results = self._model.marches[-1].scores
-		self._state['current_match'] = None
-		self._state['finished_matches'].append(match_results)
+		with self._lock:
+			match_results = self._model.marches[-1].scores
+			self._state['current_match'] = None
+			self._state['finished_matches'].append(match_results)
 
 	def _start_matches(self):
-		self._model.status = 'in_progress'
-		self._model.save()
-		self._state['leaderboard'] = {player: 0 for player in self.players}
-		while self._is_active:
-			while self._state['pending_matches']:
-				match = self._state['pending_matches'].pop(0)
+		with self._lock:
+			self._model.status = 'in_progress'
+			self._model.save()
+			self._state['leaderboard'] = {player: 0 for player in self.players}
+		while True:
+			with self._lock:
+				if not self._is_active:
+					break
+			while True:
+				with self._lock:
+					if not self._state['pending_matches']:
+						break
+					match = self._state['pending_matches'].pop(0)
 				if match[1] is None:
 					break
 				self._start_match(match)
-				match_id = self._state['current_match']
+				with self._lock:
+					match_id = self._state['current_match']
 				while True:
 					if game_manager.get_game(PongHandler, match_id)._model.status == 'finished':
 						self._update_game_state(match)
@@ -85,23 +100,26 @@ class PongTournamentHandler(TournamentHandlerBase):
 					sleep(1)
 			self._set_next_matches(match)
 			self._send_state()
-		self._model.status = 'finished'
-		self._model.save()
+		with self._lock:
+			self._model.status = 'finished'
+			self._model.save()
 
 	def _generate_match_id(self):
 		letters = string.ascii_letters + string.digits
 		match_id = ''.join(random.choice(letters) for i in range(10))
 		while game_manager.get_game(PongHandler, match_id).players != []:
 			match_id = ''.join(random.choice(letters) for i in range(10))
-		self._model.matches.add(game_manager.get_game(PongHandler, match_id)._model)
-		self._model.save()
+		with self._lock:
+			self._model.matches.add(game_manager.get_game(PongHandler, match_id)._model)
+			self._model.save()
 		return match_id
 
 	def _start_match(self, match: list[Player, Player]): # type: ignore
 		match_id = self._generate_match_id()
 		player_1, player_2 = match
-		self._state['current_match'] = match_id
-		self._send_func(json.dumps({
-			'message': f'Match between {player_1} and {player_2} starting...',
-			'match_id': match_id
-		}))
+		with self._lock:
+			self._state['current_match'] = match_id
+			self._send_func(json.dumps({
+				'message': f'Match between {player_1} and {player_2} starting...',
+				'match_id': match_id
+			}))
