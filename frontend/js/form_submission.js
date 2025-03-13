60cb7e0c8c69bd6de64f41da13c144d647b9e1bb
JFikents <138692812+JorFik@users.noreply.github.com>
2025-03-13 16:34:29 +0100

    Fixed Function RegisterUser
    WIP working signup page

--- frontend/js/form_submission.js
+++ frontend/js/form_submission.js
@@ -1,15 +1,28 @@
-async function registerUser(username, firstname, lastname, email, password)
+async function registerUser(username, first_name, last_name, email, password)
 {
-	console.log("Form submitted:", { username, firstname, lastname, email, password });
+	console.log("Form submitted:", username, first_name, last_name, email, password );
 
-	const response = await fetch('http://localhost:8000/api/player/register/', {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ username, firstname, lastname, email, password })
-	});
-	if (response.ok)
-		console.log(`user ${username} registered successfully!`);
-	else
-		console.error("Error:", await response.json());
+	const form_data = new FormData();
+	form_data.append('username', username);
+	form_data.append('first_name', first_name);
+	form_data.append('last_name', last_name);
+	form_data.append('email', email);
+	form_data.append('password', password);
+
+	console.log("Form data:", form_data);
+	try {
+		const response = await fetch('https://localhost/api/player/register/', {
+			method: 'POST',
+			headers: { 'Host' : 'localhost' },
+			body: form_data,
+			credentials: 'same-origin'
+		});
+		if (response.ok)
+			console.log(`user ${username} registered successfully!`);
+		else
+			console.error("Error:", await response.json());
+	} catch (error) {
+		console.error(error);
+	}
 }
 window.registerUser = registerUser;
