b585e9ce76f36835ab45e293a1aa3095a3ae7995
42ajakob <ajakob@student.42heilbronn.de>
2025-03-25 18:00:42 +0100

    Removed unnecessary comments

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
 const MatchScore = artifacts.require("MatchScore");
 const fs = require('fs');
 
-// const Web3 = require('web3');
-// const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
-
 module.exports = function(deployer) {
 	console.log("START");
 	deployer.deploy(MatchScore, 0)
