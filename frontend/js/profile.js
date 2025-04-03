680b294118b0025cf2ef0633c4d74f1d5d2fae8c
JFikents <138692812+JorFik@users.noreply.github.com>
2025-04-03 18:37:50 +0200

    Added profile stats

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,8 +1,9 @@
 import { authenticatedFetch } from './authentication.js';
 
+console.log("Profile JS pre-loaded");
 window.initProfile = initProfile;
 
-console.log("Profile JS pre-loaded");
+let current_user = null;
 
 export function initProfile() {
 	if (document.readyState === 'complete') {
@@ -29,6 +30,7 @@ function setProfileData(data) {
 	const profile_container = document.getElementById('profile_container');
 
 	username.textContent = data.username;
+	current_user = data.username;
 	if (data.profile_picture)
 		profile_pic.src = "https://localhost" + data.profile_picture;
 	profile_container.classList.remove('loading');
@@ -52,5 +54,49 @@ async function LoadDataFromBackend(url, setter) {
 }
 
 function setStats(data) {
-	console.log(data);
+	const stats_container = document.getElementById('stats_container');
+	const games_played = document.getElementById('games_played');
+	const games_won = document.getElementById('games_won');
+	const games_lost = document.getElementById('games_lost');
+	const win_rate = document.getElementById('win_rate');
+	const user = current_user || localStorage.getItem('username');
+
+	if (data.length === 0) {
+		stats_container.innerHTML = "<p>No matches played yet.</p>";
+		stats_container.classList.remove('loading');
+		return ;
+	}
+	data.forEach(match => {
+		const winner = getPongWinner(match.scores);
+		if (winner[0] === user) {
+			games_won.textContent = parseInt(games_won.textContent) + 1;
+		}
+		else if (winner[0] !== "Draw") {
+			games_lost.textContent = parseInt(games_lost.textContent) + 1;
+		}
+		games_played.textContent = parseInt(games_played.textContent) + 1;
+	});
+	win_rate.textContent = getPongWinRate(
+		parseInt(games_won.textContent),
+		parseInt(games_played.textContent)
+	);
+	stats_container.classList.remove('loading');
+}
+
+function getPongWinner(scores) {
+	const	[player1, player2] = Object.entries(scores);
+	const	[player1_id, player1_score] = player1;
+	const	[player2_id, player2_score] = player2
+
+	if (player1_score === player2_score)
+		return ["Draw", player1_score];
+	if (player1_score < player2_score)
+		return [player2_id, player2_score];
+	return [player1_id, player1_score];
+}
+
+function getPongWinRate(wins, total) {
+	if (total === 0)
+		return 0;
+	return `${((wins / total) * 100).toFixed(2)}%`;
 }
