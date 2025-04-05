98ad030e9c11fb36e854f9e1a11cd7f99af3cc1e
42ajakob <ajakob@student.42heilbronn.de>
2025-04-05 23:45:04 +0200

    Code clean up

--- frontend/js/friends.js
+++ frontend/js/friends.js
@@ -15,125 +15,6 @@ function removeErrorMessage(input)
 	existingErrors.forEach(error => error.remove());
 }
 
-async function friendList() {
-	try {
-		const response = await authenticatedFetch('https://localhost/api/player/' + localStorage.getItem("user_id"));
-		const data = await response.json();
-		const friendsListElement = document.getElementById('friendsList');
-		console.log(data);
-
-		if (friendsListElement) {
-			friendsListElement.innerHTML = '';
-
-			if (data.friends) {
-				for (const friend of data.friends) {
-					const friend_response = await authenticatedFetch('https://localhost/api/player/' + friend);
-					
-					const friend_data = await friend_response.json();
-					const friendListItem = document.createElement('li');
-
-					const friendHTML = `
-						<div>
-						<span>${friend_data.username}</span>
-						<button id="unfriend-button" class="btn btn-danger unfriend-button" data-user-id="${friend}">Unfriend</button>
-						</div>
-					`;
-					friendListItem.innerHTML = friendHTML;
-					friendsListElement.appendChild(friendListItem);
-
-					const unfriendButton = friendListItem.querySelector('#unfriend-button');
-					unfriendButton.addEventListener('click', () => {
-						unfriend(friend);
-					});
-				};
-			} else
-				console.log('No friends found');
-		} else
-			console.error('Element with ID "friendsList" not found.');
-	} catch (error) {
-		console.error('Error fetching friend list:', error);
-	}
-}
-
-async function friendRequests() {
-	try {
-		const response = await authenticatedFetch('https://localhost/api/player/' + localStorage.getItem("user_id"));
-		const data = await response.json();
-		const requestsListElement = document.getElementById('requestsList');
-
-		if (requestsListElement) {
-			requestsListElement.innerHTML = '';
-
-			if (data.friend_requests_received) {
-				data.friend_requests_received.forEach((request) => {
-					const requestListItem = document.createElement('li');
-					requestListItem.classList.add('list-group-item');
-
-					const requestHTML = `
-					<div class="d-flex justify-content-between align-items-center">
-						<span>${request.username}</span>
-						<div class="d-flex justify-content-end">
-							<button class="btn btn-success accept-button me-2" data-user-id="${request.id}">Accept</button>
-							<button class="btn btn-danger decline-button" data-user-id="${request.id}">Decline</button>
-						</div>
-					</div>
-					`;
-					requestListItem.innerHTML = requestHTML;
-
-					requestsListElement.appendChild(requestListItem);
-				});
-			} else
-				console.log('No friend requests found');
-		} else
-			console.error('Element with ID "requestsList" not found.');
-	} catch (error) {
-		console.error('Error fetching friend requests:', error);
-	}
-}
-
-async function addFriend(username, inputUsername) {
-	if (!username) {
-		console.error('Username is required');
-		return false;
-	}
-
-	fetch(`https://localhost/api/player/list?username=${username}`, { method: 'GET' })
-	.then(response => response.json())
-	.then(data => {
-		if (data.length > 0 && data[0]) {
-			const playerHTML = `
-			<div class="d-flex justify-content-between align-items-center mb-3 p-3 border border-purple rounded shadow-sm">
-			<span class="fs-5 fw-semibold">${data[0].username}</span>
-			<div class="d-flex">
-				<button class="btn btn-purple btn-sm" id="sendRequestButton" data-player-id="${data[0].id}">Send Request</button>
-			</div>
-			</div>
-
-			`;
-
-			const playerElement = document.createElement('div');
-			playerElement.innerHTML = playerHTML;
-			const parentElement = document.getElementById('add-friends');
-			parentElement.appendChild(playerElement);
-
-			const sendRequestButton = playerElement.querySelector('#sendRequestButton');
-			sendRequestButton.addEventListener('click', () => {
-				const user_id = sendRequestButton.dataset.playerId;
-				sendRequest(user_id);
-				playerElement.remove();
-			});
-		} else
-		{
-			 inputUsername.classList.add('is-invalid');
-			showErrorMessage( inputUsername, 'This user does not exist');
-			console.log('Player not found');
-		}
-	})
-	.catch(error => {
-		console.error('Error adding friend:', error);
-	});
-}
-
 async function sendRequest(user_id) {
 	try {
 		await authenticatedFetch(`https://localhost/api/player/send_request/${user_id}/`, {
@@ -172,42 +53,175 @@ async function unfriend(user_id) {
 		const response = await authenticatedFetch(`https://localhost/api/player/unfriend/${user_id}/`, {
 			method: 'POST'
 		});
-		console.log(response);
 		console.log('Friend removed');
 	} catch (error) {
 		console.error('Error removing friend:', error);
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+async function fetchFriendData() {
+	try {
+		const response = await authenticatedFetch(`https://localhost/api/player/${localStorage.getItem("user_id")}`);
+		return await response.json();
+	} catch (error) {
+		console.error('Error fetching friend data:', error);
+	}
+}
+
+async function renderFriendList(data) {
+	const friendsListElement = document.getElementById('friendsList');
+	if (!friendsListElement)
+		return;
+
+	friendsListElement.innerHTML = '';
+
+	if (!data.friends)
+		return;
+
+	data.friends.forEach(async (friend) => {
+		const friendData = await fetchFriendDetails(friend);
+		if (!friendData)
+			return;
+
+		const friendListItem = document.createElement('li');
+		friendListItem.innerHTML = `
+			<div>
+				<span>${friendData.username}</span>
+				<button id="unfriend-button" class="btn btn-danger unfriend-button" data-user-id="${friend}">Unfriend</button>
+			</div>
+		`;
+		friendsListElement.appendChild(friendListItem);
+
+		const unfriendButton = friendListItem.querySelector('#unfriend-button');
+		unfriendButton.addEventListener('click', () => {
+			unfriend(friend);
+		});
+	});
+}
+
+async function fetchFriendDetails(friendId) {
+	try {
+		const response = await authenticatedFetch(`https://localhost/api/player/${friendId}`);
+		return await response.json();
+	} catch (error) {
+		console.error('Error fetching friend details:', error);
+	}
+}
+
+async function friendList() {
+	const data = await fetchFriendData();
+	if (!data)
+		return;
+
+	await renderFriendList(data);
+}
+
+async function renderFriendRequests(data) {
+	const requestsListElement = document.getElementById('requestsList');
+	if (!requestsListElement)
+		return;
+
+	requestsListElement.innerHTML = '';
+
+	if (!data.friend_requests_received)
+		return;
+
+	data.friend_requests_received.forEach((request) => {
+		const requestListItem = document.createElement('li');
+		requestListItem.classList.add('list-group-item');
+
+		const requestHTML = `
+			<div class="d-flex justify-content-between align-items-center">
+				<span>${request.username}</span>
+				<div class="d-flex justify-content-end">
+					<button class="btn btn-success accept-button me-2" data-user-id="${request.id}">Accept</button>
+					<button class="btn btn-danger decline-button" data-user-id="${request.id}">Decline</button>
+				</div>
+			</div>
+		`;
+		requestListItem.innerHTML = requestHTML;
+
+		requestsListElement.appendChild(requestListItem);
+
+		const acceptButton = requestListItem.querySelector('.accept-button');
+		const declineButton = requestListItem.querySelector('.decline-button');
+
+		acceptButton.addEventListener('click', () => {
+			acceptRequest(request.id);
+		});
+
+		declineButton.addEventListener('click', () => {
+			declineRequest(request.id);
+		});
+	});
+}
+
+async function friendRequests() {
+	try {
+		const response = await authenticatedFetch(`https://localhost/api/player/${localStorage.getItem("user_id")}`);
+		const data = await response.json();
+		await renderFriendRequests(data);
+	} catch (error) {
+		console.error('Error fetching friend requests:', error);
+	}
+}
+
+async function addFriend(username, inputUsername) {
+	try {
+		const response = await fetch(`https://localhost/api/player/list?username=${username}`, { method: 'GET' });
+		const data = await response.json();
+
+		if (!data.length || !data[0]) {
+			inputUsername.classList.add('is-invalid');
+			showErrorMessage(inputUsername, 'User does not exist');
+			console.log('Player not found');
+			return;
+		}
+
+		const playerHTML = `
+			<div class="d-flex justify-content-between align-items-center mb-3 p-3 border border-purple rounded shadow-sm">
+				<span class="fs-5 fw-semibold">${data[0].username}</span>
+				<div class="d-flex">
+					<button class="btn btn-purple btn-sm" id="sendRequestButton" data-player-id="${data[0].id}">Send Request</button>
+				</div>
+			</div>
+		`;
+
+		const playerElement = document.createElement('div');
+		playerElement.innerHTML = playerHTML;
+
+		const parentElement = document.getElementById('add-friends');
+		parentElement.appendChild(playerElement);
+
+		const sendRequestButton = playerElement.querySelector('#sendRequestButton');
+		sendRequestButton.addEventListener('click', () => {
+			const user_id = sendRequestButton.dataset.playerId;
+			sendRequest(user_id);
+			playerElement.remove();
+		});
+	} catch (error) {
+		console.error('Error adding friend:', error);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
 	const friendsTabElement = document.getElementById('friends-tab');
 	const requestsTabElement = document.getElementById('requests-tab');
 	const addFriendForm = document.getElementById('addFriendForm');
-	const requestsListElement = document.getElementById('requestsList');
-	const inputUsername  = document.getElementById('newFriendName');
 
-	addFriendForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-		removeErrorMessage(inputUsername);
-		const username = inputUsername.value.trim();
-		if (username)
-			addFriend(username, inputUsername);
-		else
-			document.getElementById('addFriendFeedback').innerHTML = 'Please enter a username';
-	});
-
-	requestsListElement.addEventListener('click', async (event) => {
-		const response = await authenticatedFetch('https://localhost/api/player/' + localStorage.getItem("user_id"));
-		const data = await response.json();
-		const user_id = data.friend_requests_received.find(request => request.id == event.target.dataset.userId).id;
-
-		if (event.target.classList.contains('accept-button')) {
-			acceptRequest(user_id);
-		} else if (event.target.classList.contains('decline-button')) {
-			declineRequest(user_id);
-		}
-	});
-
+	addFriendForm.addEventListener('submit', handleAddFriendFormSubmit);
 	friendsTabElement.addEventListener('click', friendList);
 	requestsTabElement.addEventListener('click', friendRequests);
 });
+
+function handleAddFriendFormSubmit(e) {
+	e.preventDefault();
+	const inputUsername = document.getElementById('newFriendName');
+	removeErrorMessage(inputUsername);
+	const username = inputUsername.value.trim();
+	if (username) {
+		addFriend(username, inputUsername);
+	} else {
+		document.getElementById('addFriendFeedback').innerHTML = 'Please enter a username';
+	}
+}
