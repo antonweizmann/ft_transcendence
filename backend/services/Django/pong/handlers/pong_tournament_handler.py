a10968df578ece3604716893dac07a3f7230ce4d
JFikents <138692812+JorFik@users.noreply.github.com>
2025-04-16 13:57:08 +0200

    Fix problems with DB
    maybe

--- backend/services/Django/pong/handlers/pong_tournament_handler.py
+++ backend/services/Django/pong/handlers/pong_tournament_handler.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model # type: ignore
+from django.db import transaction # type: ignore
 from game_base.handlers import TournamentHandlerBase
 from pong.models import PongTournamentModel
 from pong.handlers import PongGameHandler
@@ -19,7 +20,8 @@ class PongTournamentHandler(TournamentHandlerBase):
 		self.set_name("Tournament " + self._id.removesuffix('_tournament_pong'))
 		self.set_description("Pong Tournament " + self._id.removesuffix('_tournament_pong') + ", let's play!")
 		self._model.lobby_id = self._id.removesuffix('_tournament_pong')
-		self._model.save()
+		transaction.on_commit(lambda: self._model.save())
+		self._model.refresh_from_db()
 
 	def _set_matches(self, players_without_match: list[str]):
 		random.shuffle(players_without_match)
